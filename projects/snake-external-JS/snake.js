9c0ab6e1e4eee7c031629628a62a4a48426d2791
Mikayla Hutchinson <81061290+MikaylaHutchinson@users.noreply.github.com>
2021-05-02 00:24:44 -0400

    fixed issue

--- projects/snake-external-JS/snake.js
+++ projects/snake-external-JS/snake.js
@@ -1,11 +1,12 @@
 import {getInputDirection} from './input.js'
 
-export const snakeSpeed=3
+export const snakeSpeed=4
 const snakeBody= [{ x: 11, y: 11}]
 let newSegments=0
 
 export function update() {
   addSegments()
+
   const inputDirection=getInputDirection()
   for (let i=snakeBody.length-2; i>=0; i--) {
     snakeBody[i+1]={ ...snakeBody[i]}
@@ -50,7 +51,7 @@ function equalPositions(pos1, pos2) {
 }
 
 function addSegments() {
-  for (let i=0; i=newSegments; i++) {
+  for (let i=0; i<newSegments; i++) {
     snakeBody.push({ ...snakeBody[snakeBody.length-1] })
   }
   newSegments=0
