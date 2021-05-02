22d9f52b2a33a94e7e092a431c7a080d1b59b16b
Mikayla Hutchinson <81061290+MikaylaHutchinson@users.noreply.github.com>
2021-05-01 22:41:47 -0400

    Update snake.js

--- projects/snake-external-JS/snake.js
+++ projects/snake-external-JS/snake.js
@@ -1,4 +1,4 @@
-import {getInputDirection} from './input.js'
+import {getInputDirection} from 'http://localhost:8000/snake-external-JS/input.js'
 
 export const snakeSpeed=2
 const snakeBody= [{ x: 11, y: 11}]
