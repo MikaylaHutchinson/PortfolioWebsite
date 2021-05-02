40e132ee6d2a39121b24e71a8bc5520bb7e4f47b
Mikayla Hutchinson <81061290+MikaylaHutchinson@users.noreply.github.com>
2021-05-01 23:31:45 -0400

    grid

--- projects/snake-external-JS/grid.js
+++ projects/snake-external-JS/grid.js
@@ -4,7 +4,7 @@ const gridSize=21
 export function randomGridPosition() {
     return {
         x: Math.floor(Math.random() * gridSize)+1,
-        y: Math.floor(Math.random() * gridSize)+1,
+        y: Math.floor(Math.random() * gridSize)+1
     }
 }
 
