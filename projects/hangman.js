724c6d2ada9dfae51388c1faa523f058a88aec6d
Mikayla Hutchinson <81061290+MikaylaHutchinson@users.noreply.github.com>
2021-07-03 03:17:01 -0400

    added function to category buttons

--- projects/hangman.js
+++ projects/hangman.js
@@ -1,15 +1,53 @@
 var chooseCat = document.getElementsByClassName('hangman-choosecategory')[0];
 var oneplayerbtn = document.getElementsByClassName('hangman-1player')[0];
 var twoplayerbtn = document.getElementsByClassName('hangman-2players')[0];
+var playerbtns = document.getElementsByClassName('hangman-playerbtns')[0]
+var categories = document.getElementsByClassName('hangman-categoriescontainer')[0]
 
 
 chooseCat.style.display = 'none';
-
-
+categories.style.display = 'none';
 
 
 function oneplayerselect() {
     chooseCat.style.display = 'block';
-    oneplayerbtn.style.display = 'none';
-    twoplayerbtn.style.display = 'none';   
+    categories.style.display = 'block';
+    playerbtns.style.display = 'none';   
+}
+
+
+var movieinstructions = document.getElementsByClassName('hangman-movieinstructions')[0]
+var bookinstructions = document.getElementsByClassName('hangman-bookinstructions')[0]
+var songinstructions = document.getElementsByClassName('hangman-songinstructions')[0]
+var phraseinstructions = document.getElementsByClassName('hangman-phraseinstructions')[0]
+
+
+movieinstructions.style.display = 'none';
+bookinstructions.style.display = 'none';
+songinstructions.style.display = 'none';
+phraseinstructions.style.display = 'none';
+
+
+function movieselect() {
+    categories.style.display = 'none';
+    chooseCat.style.display = 'none';
+    movieinstructions.style.display = 'block';
+}
+
+function bookselect() {
+    categories.style.display = 'none';
+    chooseCat.style.display = 'none';
+    bookinstructions.style.display = 'block';
+}
+
+function songselect() {
+    categories.style.display = 'none';
+    chooseCat.style.display = 'none';
+    songinstructions.style.display = 'block';
+}
+
+function phraseselect() {
+    categories.style.display = 'none';
+    chooseCat.style.display = 'none';
+    phraseinstructions.style.display = 'block';
 }
